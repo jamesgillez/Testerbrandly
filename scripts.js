b0d76989a9dbcb9aa8435e86cec7f638ddae1770
jamesgillez <jam423367@gmail.com>
2025-05-05 21:49:14 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
-console.log('Website is working');
+
+document.addEventListener("DOMContentLoaded", function () {
+  const carousel = document.querySelector(".carousel-inner");
+  const nextBtn = document.querySelector(".carousel-btn.next");
+  const prevBtn = document.querySelector(".carousel-btn.prev");
+  const images = document.querySelectorAll(".carousel-inner img");
+
+  let index = 0;
+
+  function updateCarousel() {
+    const width = images[0].clientWidth;
+    carousel.style.transform = `translateX(-${index * width}px)`;
+  }
+
+  nextBtn.addEventListener("click", () => {
+    index = (index + 1) % images.length;
+    updateCarousel();
+  });
+
+  prevBtn.addEventListener("click", () => {
+    index = (index - 1 + images.length) % images.length;
+    updateCarousel();
+  });
+
+  window.addEventListener("resize", updateCarousel);
+  updateCarousel();
+});
